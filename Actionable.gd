a8e1ea8fa41622631338aac300701028a85f0d17
JumpyJimmy <118216774+JumpyJimmy@users.noreply.github.com>
2023-12-04 16:14:13 +0000

    added background image for start scene, and portraits for all player dialogues

--- Actionable.gd
+++ Actionable.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
-#const Balloon = preload("res://Dialogue/balloon.tscn")
+const Balloon = preload("res://Dialogue/balloon.tscn")
 
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
 @export var dialogue_over: String = "over"
 
 func action() -> void:
-	#var balloon : Node = Balloon.instantiate()
-	#get_tree().current_scene.add_child(balloon)
-	#balloon.start(dialogue_resource, dialogue_start)
+	var balloon : Node = Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(dialogue_resource, dialogue_start)
 	'''
 	if global.has_lost != true:
 		DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
@@ -20,9 +20,9 @@ func action() -> void:
 	'''
 	log(global.has_lost)
 	#DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
-	var balloon = load("res://Dialogue/balloon.tscn").instantiate()
-	get_tree().current_scene.add_child(balloon)
-	balloon.start(load("res://Dialogue/test.dialogue"), "start")
+	#var balloon = load("res://Dialogue/balloon.tscn").instantiate()
+	#get_tree().current_scene.add_child(balloon)
+	#balloon.start(load("res://Dialogue/test.dialogue"), "start")
 	if global.has_lost == true:
 		await get_tree().create_timer(5.5).timeout
 		get_tree().quit()
